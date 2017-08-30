8e1a9bcd3231e3b60e6b9c7b2d7610b1ac3b6c65
mlkumanova <m_kumanova@abv.bg>
2017-08-30 21:07:46 +0300

    basic structure

---
+++ js/MK functions.js
@@ -0,0 +1,12 @@
+$('#jquery').animate({
+    width: "90%"
+}, 2000)
+$('#javaScript').animate({
+    width: "50%"
+}, 2000)
+$('#programming').animate({
+    width: "80%"
+}, 2000)
+$('#design').animate({
+    width: "10%"
+}, 2000)

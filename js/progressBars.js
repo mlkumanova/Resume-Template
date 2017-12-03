e6c9577b656f8843d228e5e50f4dde964c9fff9f
Maria <m_kumanova@abv.bg>
2017-12-03 15:05:20 +0200

    final

--- js/progressBars.js
+++ js/progressBars.js
@@ -10,3 +10,12 @@ $('#programming').animate({
 $('#design').animate({
     width: "10%"
 }, 2000)
+$('#spanishLanguage').animate({
+    width: "70%"
+}, 2000)
+$('#englishLanguage').animate({
+    width: "50%"
+},2000)
+$('#russianLanguage').animate({
+    width: "80%"
+}, 2000)
